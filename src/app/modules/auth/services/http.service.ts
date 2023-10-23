88f907c923fd415c4d0ce58b178e67b26be67272
Eşref Atak <esrefatak@gmail.com>
2023-10-23 21:11:38 +0300

    cache ve zoom çözüldü

--- src/app/modules/auth/services/http.service.ts
+++ src/app/modules/auth/services/http.service.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-import { UserModel } from '../models/user.model';
-import { AuthModel } from '../models/auth.model';
-import { UsersTable } from '../../../_fake/users.table';
+import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 
 const API_USERS_URL = `${environment.apiUrl}/users`;
@@ -22,7 +17,7 @@ export class HTTPService {
       headers = headers.set("Authorization", "Bearer " + localStorage.getItem("token"));
     }
     return this.http.post(
-      "https://alb-recommed-1454511849.eu-west-1.elb.amazonaws.com/api/" + url,
+      "https://api.recommed.co/api/" + url,
       payload,
       {
         headers: headers
@@ -30,40 +25,4 @@ export class HTTPService {
     )
   }
 
-  createUser(user: UserModel): Observable<any> {
-    user.roles = [2]; // Manager
-    user.authToken = 'auth-token-' + Math.random();
-    user.refreshToken = 'auth-token-' + Math.random();
-    user.expiresIn = new Date(Date.now() + 100 * 24 * 60 * 60 * 1000);
-    user.pic = './assets/media/avatars/300-1.jpg';
-
-    return this.http.post<UserModel>(API_USERS_URL, user);
-  }
-
-  forgotPassword(email: string): Observable<boolean> {
-    return this.getAllUsers().pipe(
-      map((result: UserModel[]) => {
-        const user = result.find(
-          (u) => u.email.toLowerCase() === email.toLowerCase()
-        );
-        return user !== undefined;
-      })
-    );
-  }
-
-  getUserByToken(token: string): Observable<UserModel | undefined> {
-    const user = UsersTable.users.find((u: UserModel) => {
-      return u.authToken === token;
-    });
-
-    if (!user) {
-      return of(undefined);
-    }
-
-    return of(user);
-  }
-
-  getAllUsers(): Observable<UserModel[]> {
-    return this.http.get<UserModel[]>(API_USERS_URL);
-  }
 }
