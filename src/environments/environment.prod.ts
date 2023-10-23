5a5d7d012416dd2e06f065023537b85dda893134
Eşref Atak <esrefatak@gmail.com>
2023-10-23 23:11:43 +0300

    callback is ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
   appVersion: 'v8.1.8',
   USERDATA_KEY: 'token',
   isMockEnabled: true,
-  apiUrl: 'https://recommed.co/api/',
+  apiUrl: 'https://api.recommed.co/api/',
   appThemeName: 'Metronic',
   appPurchaseUrl: 'https://1.envato.market/EA4JP',
   appHTMLIntegration:
